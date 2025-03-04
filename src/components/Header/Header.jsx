9b4129bf4e8f924c8067a3af3a58cca63155dcb6
danylo.nabok <danylo.nabok@gr8.tech>
2025-03-04 21:13:29 +0200

    feat: add first block

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+import "./Header.css";
+
+export const Header = () => (
+  <div className="wrapper">
+    <div className="wrapperCover">
+      <header className="headerWrapper">
+        <ul>
+          <li>
+            <a href="/">Про автора</a>
+          </li>
+          <li>
+            <a href="#history">Про книгу</a>
+          </li>
+          <li>
+            <a href="/">Контакти</a>
+          </li>
+        </ul>
+      </header>
+
+      <div className="headerContentWrapper">
+        <h1>Катрусина читальня</h1>
+        <h3>Простір, де живуть улюблені історії</h3>
+
+        <a href="#history">Про книгу</a>
+      </div>
+    </div>
+  </div>
+);
---
+++ src/components/HistoryBlock/HistoryBlock.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./HistoryBlock.css";
+
+export const HistoryBlock = () => {
+  return (
+    <div className="historyBlockWrapper" id="history">
+      <h3>Історія книги</h3>
+    </div>
+  );
+};
